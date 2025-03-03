d1bc2e9dd922515cf479019e7424205afcc1468a
theosiemensrhodes <theo.siemensrhodes@gmail.com>
2025-03-02 21:38:50 -0800

    Fix compilation on mac

--- Source/App/tl26_functions.c
+++ Source/App/tl26_functions.c
@@ -1,24 +1,25 @@
 #include "tl26_flags.h"
+#include "svt_log.h"
 
 void initialize_python() {
-    // Initialize the Python interpreter
     Py_Initialize();
+
+    // Convert module name to a Python object
     PyObject *pName = PyUnicode_DecodeFSDefault("tl26.utils");
+    if (!pName) {
+        PyErr_Print();
+        panic("Error: Failed to decode module name 'tl26.utils'.\n");
+    }
+
+    // Import the module
     PyObject *pModule = PyImport_Import(pName);
     Py_DECREF(pName);
-    PyObject *python_greetings = PyObject_GetAttrString(pModule, "hello_SVTAV1");
-    if (PyCallable_Check(python_greetings)) {
-        PyObject *pValue = PyObject_CallObject(python_greetings, NULL);
-        if (pValue != NULL) {
-            Py_DECREF(pValue);
-        } else {
-            PyErr_Print();
-        }
-    } else {
+    if (!pModule) {
         PyErr_Print();
+        panic(stderr, "Error: Failed to import module 'tl26.utils'.\n");
     }
-    Py_XDECREF(python_greetings);
-    Py_DECREF(pModule);
+
+    SVT_INFO("Successfully loaded tl26 module!");
 }
 
 void finalize_python() {
