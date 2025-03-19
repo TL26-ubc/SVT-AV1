8c343f6bb65237d9de7b0dd1a44f0fcbcd68df3b
Nola <yyyjiayue@outlook.com>
2025-03-18 23:58:00 -0700

    Apply local changes as a single commit

--- Source/tl26/tl26_functions.c
+++ Source/tl26/tl26_functions.c
@@ -1,61 +1,26 @@
 #include "tl26_flags.h"
+#include "tl26_python_thread.h"
 
 #ifdef TL26_RL
 static PyObject *f_frame_report_feedback, *f_sb_send_offset_request;
-
-static void initialize_communications() {
-    // import the functions from tl.feedback
-    PyObject *pFeedbackName   = PyUnicode_DecodeFSDefault("tl26.feedback");
-    PyObject *pFeedbackModule = PyImport_Import(pFeedbackName);
-    Py_DECREF(pFeedbackName);
-    f_frame_report_feedback = PyObject_GetAttrString(pFeedbackModule, "frame_report_feedback");
-    Py_DECREF(pFeedbackModule);
-
-    // import the functions from tl.request
-    PyObject *pRequestName   = PyUnicode_DecodeFSDefault("tl26.request");
-    PyObject *pRequestModule = PyImport_Import(pRequestName);
-    Py_DECREF(pRequestName);
-    f_sb_send_offset_request = PyObject_GetAttrString(pRequestModule, "sb_send_offset_request");
-    Py_DECREF(pRequestModule);
-}
+static PyThreadState *main_thread_state = NULL;
 
 void initialize_python() {
-    // Initialize the Python interpreter
     Py_Initialize();
-
-    PyGILState_STATE state = PyGILState_Ensure();
-
-    PyObject *pName   = PyUnicode_DecodeFSDefault("tl26.utils");
-    PyObject *pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-    PyObject *python_greetings = PyObject_GetAttrString(pModule, "hello_SVTAV1");
-
-
-    if (PyCallable_Check(python_greetings)) {
-        PyObject *pValue = PyObject_CallObject(python_greetings, NULL);
-        if (pValue != NULL) {
-            Py_DECREF(pValue);
-        } else {
-            PyErr_Print();
-        }
-    } else {
-        PyErr_Print();
-    }
-    Py_XDECREF(python_greetings);
-    Py_DECREF(pModule);
-
-    // Initialize the communication functions for SVT-AV1 and Python
-    initialize_communications();
-
-    PyGILState_Release(state);
+    main_thread_state = PyThreadState_Get();
+    PyEval_ReleaseThread(main_thread_state);
+    init_python_thread();
 }
 
 void finalize_python() {
-    // Free the function objects
-    Py_XDECREF(f_frame_report_feedback);
-    Py_XDECREF(f_sb_send_offset_request);
-
-    // Finalize the Python interpreter
+    set_python_thread_running(false);
+    signal_python_thread_termination();
+    shutdown_python_thread();
+    
+    usleep(300000);
+    
+    PyEval_AcquireThread(main_thread_state);
+    
     Py_Finalize();
 }
 
@@ -66,5 +31,4 @@ PyObject *get_f_frame_report_feedback() {
 PyObject *get_f_sb_send_offset_request() {
     return f_sb_send_offset_request;
 }
-
 #endif
--- Source/tl26/tl26_request.c
+++ Source/tl26/tl26_request.c
@@ -1,81 +1,39 @@
 #include "tl26_flags.h"
 #include "tl26_request.h"
+#include "tl26_python_thread.h"
 
 int request_sb_offset(SuperBlock *sb_ptr, int encoder_bit_depth, int qindex, double beta, bool slice_type_is_I_SLICE) {
-    PyObject *f_sb_send_offset_request = get_f_sb_send_offset_request();
-    if (PyCallable_Check(f_sb_send_offset_request)) {
+#ifdef TL26_RL
+    TileInfo *tile_info = &sb_ptr->tile_info;
+    unsigned  sb_index = sb_ptr->index, sb_origin_x = sb_ptr->org_x, sb_origin_y = sb_ptr->org_y;
+    int       sb_qp = (int)sb_ptr->qindex, sb_final_blk_cnt = (int)sb_ptr->final_blk_cnt;
 
-        // PyGILState_STATE state = PyGILState_Ensure();
-        TileInfo *tile_info = &sb_ptr->tile_info;
-        unsigned  sb_index = sb_ptr->index, sb_origin_x = sb_ptr->org_x, sb_origin_y = sb_ptr->org_y;
-        int       sb_qp = (int)sb_ptr->qindex, sb_final_blk_cnt = (int)sb_ptr->final_blk_cnt;
+    int tile_row = tile_info->tile_row, tile_col = tile_info->tile_col, tile_rs_index = tile_info->tile_rs_index;
+    int mi_row_start = tile_info->mi_row_start, mi_row_end = tile_info->mi_row_end,
+        mi_col_start = tile_info->mi_col_start, mi_col_end = tile_info->mi_col_end;
+    int tg_horz_boundary = tile_info->tg_horz_boundary;
 
-        int tile_row = tile_info->tile_row, tile_col = tile_info->tile_col, tile_rs_index = tile_info->tile_rs_index;
-        int mi_row_start = tile_info->mi_row_start, mi_row_end = tile_info->mi_row_end,
-            mi_col_start = tile_info->mi_col_start, mi_col_end = tile_info->mi_col_end;
-        int tg_horz_boundary = tile_info->tg_horz_boundary;
+    int type = slice_type_is_I_SLICE ? 1 : 0;
 
-        int type = slice_type_is_I_SLICE ? 1 : 0;
-
-        PyObject *ptests = Py_BuildValue(
-            "i", // this is telling the function to expect 14 arguments for int and double
-            1);
-        PyObject *argstest = Py_BuildValue(
-            "ii", // this is telling the function to expect 14 arguments for int and double
-            1,1);
-
-        PyObject *args = Py_BuildValue("IIIiiiiiiiiiiiidi",
-                                       sb_index,
-                                       sb_origin_x,
-                                       sb_origin_y,
-                                       sb_qp,
-                                       sb_final_blk_cnt, // sb
-                                       mi_row_start,
-                                       mi_row_end,
-                                       mi_col_start,
-                                       mi_col_end,
-                                       tg_horz_boundary,
-                                       tile_row,
-                                       tile_col,
-                                       tile_rs_index, // tile
-                                       encoder_bit_depth,
-                                       qindex,
-                                       beta,
-                                       type);
-
-
-        // PyObject *args      = Py_BuildValue("IIIiiiiiiiiiiiidb",
-        //                                sb_ptr->index, // unsigned
-        //                                sb_ptr->org_x, // unsigned
-        //                                sb_ptr->org_y, // unsigned
-        //                                (int) sb_ptr->qindex, // uint8_t
-        //                                (int) sb_ptr->final_blk_cnt, // uint16_t
-
-        //                                tile_info->mi_row_start, // int32_t
-        //                                tile_info->mi_row_end, // int32_t
-        //                                tile_info->mi_col_start, // int32_t
-        //                                tile_info->mi_col_end, // int32_t
-        //                                tile_info->tg_horz_boundary, // int32_t
-        //                                tile_info->tile_row, // int32_t
-        //                                tile_info->tile_col, // int32_t
-        //                                tile_info->tile_rs_index, // int32_t
-
-        //                                encoder_bit_depth, // int
-        //                                qindex, // int
-        //                                beta, // double
-        //                                slice_type_is_I_SLICE);
-        PyObject *pValue = PyObject_CallObject(f_sb_send_offset_request, args);
-        if (pValue != NULL) {
-            int offset = PyLong_AsLong(pValue);
-            Py_DECREF(pValue);
-            // PyGILState_Release(state);
-            return offset;
-        } else {
-            PyErr_Print();
-        }
-        Py_DECREF(args);
-    } else {
-        PyErr_Print();
-    }
+    return submit_sb_offset_request(
+        sb_index,
+        sb_origin_x,
+        sb_origin_y,
+        sb_qp,
+        sb_final_blk_cnt,
+        mi_row_start,
+        mi_row_end,
+        mi_col_start,
+        mi_col_end,
+        tg_horz_boundary,
+        tile_row,
+        tile_col,
+        tile_rs_index,
+        encoder_bit_depth,
+        qindex,
+        beta,
+        type);
+#else
     return -1;
+#endif
 }
