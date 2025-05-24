562bfb41e2018e7f55ebb7ecc72e96803124aaa0
Jiajun-Huang <jhuang430@qq.com>
2025-05-23 19:13:28 -0700

    temp push

--- python-gym/src/pyencoder/environment/utils.py
+++ python-gym/src/pyencoder/environment/utils.py
@@ -1,4 +1,9 @@
+import os.path
+from pathlib import Path
+from typing import Tuple
+
 import cv2
+
 
 def _probe_resolution(path: Path) -> Tuple[int, int]:
     cap = cv2.VideoCapture(path)
@@ -6,7 +11,7 @@ def _probe_resolution(path: Path) -> Tuple[int, int]:
         raise IOError(f"Cannot open video file: {path}")
 
     # Read frame width and height
-    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     cap.release()
