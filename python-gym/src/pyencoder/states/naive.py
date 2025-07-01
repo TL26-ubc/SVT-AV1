fix state y variance calculation

python-gym/src/pyencoder/states/naive.py
@@ -28,13 +28,15 @@ class NaiveState(AbstractState):
 
     def get_observation(
         self,
-        frame: np.ndarray,
+        frame: np.ndarray, # The current frame in yuv420p format, shape (3/2 * H, W).
         sbs: list[SuperBlockInfo], 
         frame_type: int,
         picture_number: int,
         **kwargs
     ) -> np.ndarray:
         h, w = frame.shape[:2]
+        assert h % 3 == 0, "Height must be a multiple of 3 for yuv420p format"
+        h, w = h // 3 * 2, w  # Adjust for yuv420p format
         y_comp_list = []
         h_mv_list = []
         v_mv_list = []
@@ -45,14 +47,16 @@ class NaiveState(AbstractState):
             for x in range(0, w, self.sb_size):  # follow encoder order, x changes first
                 y_end = min(y + self.sb_size, h)
                 x_end = min(x + self.sb_size, w)
-                sb = frame[y:y_end, x:x_end]
-                if sb.size == 0:
+                sb_y_component = frame[y:y_end, x:x_end]
+                # sb_cb_component = frame[h + y // 2:h + y_end // 2, x:x_end]
+                # sb_cr_component = frame[h + h // 2 + y // 2:h + h // 2 + y_end // 2, x:x_end]
+                if sb_y_component.size == 0:
                     continue
-
-                sb_y_var = np.var(sb[:, :, 0])  # Y-component variance
-                # sb_x_mv = np.mean(sb[:, :, 1])  # Horizontal mean value
-                # sb_y_mv = np.mean(sb[:, :, 2])  # Vertical mean value
-                # beta = np.mean(np.abs(sb))  # Example metric
+                
+                # Y-component variance
+                sb_y_var = np.var(sb_y_component)
+                # sb_cb_var = np.var(sb_cb_component)
+                # sb_cr_var = np.var(sb_cr_component)
 
                 y_comp_list.append(sb_y_var)
                 h_mv_list.append(sbs[sb_idx]['sb_x_mv'])

python-gym/src/pyencoder/utils/video_reader.py
@@ -1,17 +1,9 @@
-import csv
 import enum
 from typing import Literal, Optional, Tuple, TypeAlias, cast
-
 import av
 import cv2
 import numpy as np
 from numpy import ndarray
-
-import os
-
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.colors import LinearSegmentedColormap
 from pyencoder.environment.constants import SB_SIZE
 
 class VideoComponent(enum.Enum):
@@ -44,7 +36,7 @@ class VideoReader:
                 self._frame_count = count
                 self.container.seek(0)
 
-    def _get_frame_at_index(self, frame_number):
+    def _get_frame_at_index(self, frame_number) -> Optional[av.VideoFrame]:
         """Get PyAV frame at specific index"""
         if frame_number in self._frames_cache:
             return self._frames_cache[frame_number]
@@ -59,15 +51,14 @@ class VideoReader:
             current_index += 1
         return None
 
-    def read_frame(self, frame_number) -> Optional[np.ndarray]: # (H, W, 3)
+    def read_frame(self, frame_number) -> Optional[np.ndarray]: 
+        # The current frame in yuv420p format, shape (3/2 * H, W).
         av_frame = self._get_frame_at_index(frame_number)
         if av_frame is None:
             return None
         
-        # Convert to RGB then to BGR to match cv2 format
-        rgb_array = av_frame.to_ndarray(format='rgb24')
-        bgr_array = cv2.cvtColor(rgb_array, cv2.COLOR_RGB2BGR)
-        return bgr_array
+        # yuv420p format conversion
+        return av_frame.to_ndarray(format='yuv420p')
 
     def release(self):
         self.container.close()
@@ -76,7 +67,7 @@ class VideoReader:
     def get_resolution(self) -> Tuple[int, int]:
         return self.width, self.height
 
-    def read_ycrcb_components(self, frame_number: int) -> Optional[np.ndarray]: # (3/2 * H, W)
+    def read_ycrcb_components(self, frame_number: int) -> Optional[np.ndarray]: # (3/2 * H, W) in yuv420p format
         av_frame = self._get_frame_at_index(frame_number)
         if av_frame is None:
             return None
@@ -88,16 +79,6 @@ class VideoReader:
     def get_frame_count(self) -> int:
         self._ensure_frame_count()
         return self._frame_count
-
-    def render_frame_number(self, frame_number: int):
-        frame = self.read_frame(frame_number=frame_number)
-        if frame is not None:
-            self.render_frame(frame)
-
-    def render_frame(self, frame: np.ndarray):
-        cv2.imshow("Frame", frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
     # sb info
     def get_num_superblock(self):